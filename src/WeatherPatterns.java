bdc7611102fabed5a0ebde0d9203d9fbb964771c
AmaySrinivasan <amay.srinivasan@menloschool.org>
2026-01-15 20:28:01 -0800

    Version 1

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -16,7 +16,12 @@ public class WeatherPatterns {
      */
     public static int longestWarmingTrend(int[] temperatures) {
         // TODO: Write your code here!
-
+        // n is the length of temperatuers
+        // Set an array with entry i being the length of longest warming trend ending at node i
+        //  and the array itself is an array of size n, filled with 1s?
+        // And then created a nested for loop with i going from 1 to n and j going from 1 to i
+        // And then compare temperatures at these locations and update the lengths
+        // And then return the max value in the array because that would be the longest length
         return 0;
     }
 }
