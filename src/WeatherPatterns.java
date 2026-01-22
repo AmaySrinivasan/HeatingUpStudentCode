718d9ed32d07faf474ac2a9a9d71c49503a0f6a8
AmaySrinivasan <amay.srinivasan@menloschool.org>
2026-01-21 23:09:19 -0800

    Final Version

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -3,7 +3,7 @@
  * each day’s temperature is higher than on the previous day in that sequence.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Amay Srinivasan
  */
 
 public class WeatherPatterns {
@@ -11,17 +11,41 @@ public class WeatherPatterns {
 
     /**
      * Longest Warming Trend
+     *
      * @param temperatures
      * @return the longest run of days with increasing temperatures
      */
     public static int longestWarmingTrend(int[] temperatures) {
-        // TODO: Write your code here!
-        // n is the length of temperatuers
-        // Set an array with entry i being the length of longest warming trend ending at node i
-        //  and the array itself is an array of size n, filled with 1s?
-        // And then created a nested for loop with i going from 1 to n and j going from 1 to i
-        // And then compare temperatures at these locations and update the lengths
-        // And then return the max value in the array because that would be the longest length
-        return 0;
+        // If there's no temps, then there's no warming trend
+        if (temperatures == null || temperatures.length == 0) {
+            return 0;
+        }
+        // Stores length of longest warming trend ending on day i and stores longest trend found
+        int[] trendLength = new int[temperatures.length];
+        int answer = 1;
+        // Tries to use each day as the ending point of a warming trend (computeTrend works recursively)
+        for (int i = 0; i < temperatures.length; i++) {
+            answer = Math.max(answer, computeTrend(i, temperatures, trendLength));
+        }
+        return answer;
+    }
+
+    // Uses recursion to compute the longest trend ending on given day
+    private static int computeTrend(int day, int[] temps, int[] trendLength) {
+        // If we've already computed for this day, then just return it
+        if (trendLength[day] != 0) {
+            return trendLength[day];
+        }
+        // At the min, trend is just this day, and then check all the earlier days to see if they come before this day
+        int maxSoFar = 1;
+        for (int prev = day - 1; prev >= 0; prev--) {
+            if (temps[prev] < temps[day]) {
+                // Extends warming trend prev by including this day
+                maxSoFar = Math.max(maxSoFar, computeTrend(prev, temps, trendLength) + 1);
+            }
+        }
+        // Save the computed result so no need to recompute later
+        trendLength[day] = maxSoFar;
+        return maxSoFar;
     }
 }
